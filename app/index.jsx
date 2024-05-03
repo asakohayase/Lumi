6bb7d892a5989d049df05b1454b0488a709541d7
Asako Hayase <chipmunk.kck@gmail.com>
2024-05-02 23:29:08 -0700

    Update index.jsx and add CustomButton component

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, StatusBar, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import {images} from "../constants";
@@ -11,20 +11,20 @@ export default function App() {
         <View className="w-full flex justify-center items-center px-4">
           <Image source={images.logo} className="w-[130px] h-[84px]" resizeMode="contain"/>
           <Image source={images.cards} className="max-w-[380px] w-full h-[300px]" resizeMode="contain"/>
-        </View>
-        <View className="relative mt-5">
-          <Text className="text-3xl text-white font-bold text-center">
-            Discover Endless Possibilities with {' '}
-            <Text className="text-secondary-200">
-              Aura
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities with {' '}
+              <Text className="text-secondary-200">
+                Aura
+              </Text>
             </Text>
-          </Text>
-          <View className="absolute -right-5 -bottom-2">
-            <Image source={images.path} style={{ width: 136, height: 15 }} resizeMode="contain" />
+            <Image source={images.path} style={{ width: 136, height: 15 }} resizeMode="contain"   className="absolute -right-9 -bottom-2"/>
           </View>
-          <Text className="text-sm font-pregular text-gray100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
+          <CustomButton title="Continue with Email" handlePress={()=>{}} containerStyles="w-full mt-7"/>
         </View>
      </ScrollView>
+     <StatusBar backgroundColor="#161622" style="light"/>
     </SafeAreaView>
   );
 }
