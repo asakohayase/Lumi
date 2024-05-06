9a03901e1061b85d63d1ff91a55dce87f326d02a
Asako Hayase <chipmunk.kck@gmail.com>
2024-05-05 23:14:58 -0700

    update: add sign-up logic

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,52 +1,70 @@
-import { useState } from 'react'
-import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
-import { images } from '../../constants'
-import FormField from '../../components/FormField'
-import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router'
+import { useState } from "react";
+import { View, Text, SafeAreaView, ScrollView, Image } from "react-native";
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
 
 const SignIn = () => {
-  [form, setForm] = useState({
-    email: '',
-    password: ''
-  })
-  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submit = () => {
-
-  }
+  const submit = () => {};
 
   return (
     <SafeAreaView className="bg-primary h-full">
-     <ScrollView>
-      <View className="w-full justify-center h-full px-4 my-6">
-        <Image source={images.logo} resizeMode="contain" className="w-[115px] h-[35px]"/>
-        <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora</Text>
-     
-        <FormField 
-        title="Email"
-        value={form.email}
-        handleChangeText={(e)=>setForm({...form, email: e})}
-        otherStyles="mt-7"
-        keyboardType="email-address"/>
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
 
-        <FormField 
-        title="Password"
-        value={form.password}
-        handleChangeText={(e)=>setForm({...form, password: e})}
-        otherStyles="mt-7"
-        keyboardType="password"/>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
 
-        <CustomButton title="Sign In" handlePress={submit} containerStyles="mt-7" isLoading={isSubmitting}/>
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+            keyboardType="password"
+          />
 
-        <View className="justify-center pt-5 flex-row gap-2">
-          <Text className="text-lg text-gray-100 font-pregular">Don't have an account?</Text>
-          <Link href="sign-up" className="text-lg font-psemibold text-secondary">Sign Up</Link>
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have an account?
+            </Text>
+            <Link
+              href="sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign Up
+            </Link>
+          </View>
         </View>
-      </View>
-     </ScrollView>
+      </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
